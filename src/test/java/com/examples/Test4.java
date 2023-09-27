d9a39a608f61465922e2581d62123b061c5c19d8
way2atit <way2atit@gmail.com>
2023-09-27 11:50:30 +0530

    Test4 filr chnaged Form Ecllipse

---
+++ src/test/java/com/examples/Test4.java
@@ -0,0 +1,16 @@
+package com.examples;
+
+public class Test4 {
+	
+	
+	//   
+	
+	public static void main(String[] args) {
+		int i = 10;
+		int j =10;
+		int sum = i+j;
+		System.out.println("Sum :" +sum );
+		
+	}
+
+}
