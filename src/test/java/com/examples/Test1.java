6be1c3cedc9d403eab7bfc45a2d184ecd67425b5
way2atit <way2atit@gmail.com>
2023-09-27 11:25:54 +0530

    First Commit

---
+++ src/test/java/com/examples/Test1.java
@@ -0,0 +1,14 @@
+package com.examples;
+
+public class Test1 {
+	
+	
+	public static void main(String[] args) {
+		int i = 10;
+		int j =10;
+		int sum = i+j;
+		System.out.println("Sum :" +sum );
+		
+	}
+
+}
