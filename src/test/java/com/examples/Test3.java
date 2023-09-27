2e4b25c71687ea6546c14216b5213104a467afdc
way2atit <way2atit@gmail.com>
2023-09-27 11:41:22 +0530

    Test3 FIle chnaged

---
+++ src/test/java/com/examples/Test3.java
@@ -0,0 +1,16 @@
+package com.examples;
+
+public class Test3 {
+	
+	
+	//   
+	
+	public static void main(String[] args) {
+		int i = 10;
+		int j =10;
+		int multi = i*j;
+		System.out.println("Multi :" +multi );
+		
+	}
+
+}
