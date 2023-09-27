be03a10249743f77fc1c9eb7e6042e9922ade4a3
way2atit <way2atit@gmail.com>
2023-09-27 11:45:14 +0530

    Test3 FIle chnaged

--- src/test/java/com/examples/Test2.java
+++ src/test/java/com/examples/Test2.java
@@ -1,16 +1,18 @@
 package com.examples;
 
 public class Test2 {
-	
-	
-	//   
-	
+
+	//
+
 	public static void main(String[] args) {
 		int i = 10;
-		int j =10;
-		int sum = i+j;
-		System.out.println("Sum :" +sum );
-		
+		int j = 10;
+		int div = i / j;
+		System.out.println("div :" + div);
+
+		int reuslt = i % j;
+		System.out.println("reuslt :" + reuslt);
+
 	}
 
 }
