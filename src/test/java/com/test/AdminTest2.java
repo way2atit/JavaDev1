8c2174e1faa948fecb2b08c57e5be7bafca2601f
way2atit <97036773+way2atit@users.noreply.github.com>
2023-09-28 11:56:12 +0530

    Update AdminTest2.java

--- src/test/java/com/test/AdminTest2.java
+++ src/test/java/com/test/AdminTest2.java
@@ -2,4 +2,9 @@ package com.test;
 
 public class AdminTest2 {
 
+  System.out.println("Git HUb");
+		System.out.println("Git HUb");
+		System.out.println("Git HUb");
+		System.out.println("Git HUb");
+
 }
